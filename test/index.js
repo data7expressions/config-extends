19fed4e929a4a93a813b8cc87fde6bcb1d4a79e9
flaviolrita <frita@beesion.com>
2020-12-09 07:01:22 +0100

    change strategies load files

--- test/index.js
+++ test/index.js
@@ -2,15 +2,26 @@ const path = require('path');
 const configExtended = require('../lib/config-extended');
 
 (async () => { 
-    try {        
-        
-        let config = await configExtended.loadPath(path.join(__dirname,'config'));
+    try {      
 
-        let list = []
-        for(let k in config.game){
-            list.push(config.game[k]);
-        }
-        console.log(JSON.stringify(list));
+
+        // let config = await configExtended.load(path.join(__dirname,'raspberry.yaml'));
+        // console.log(JSON.stringify(config.version));
+        
+        // console.log(path.join(path.parse('a/b/c.yaml').dir,path.parse('a/b/c.yaml').name));
+        // console.log(path.join(path.parse('c.yaml').dir,path.parse('c.yaml').name));
+
+
+        // path.parse('a/b/c.yaml').name;
+        // path.parse('a/b/c.yaml').dir;
+        
+        let config = await configExtended.load(path.join(__dirname,'tetris'));
+        console.log(JSON.stringify(config));
+        // let list = []
+        // for(let k in config.version){
+        //     list.push(config.version[k]);
+        // }
+        // console.log(JSON.stringify(list));
     }
     catch (error) {     
         console.error(error);  
