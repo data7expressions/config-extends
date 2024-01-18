3e0e6278289f11cd39494931f952b1c9c9af3f10
FlavioLionelRita <flaviolrital@hotmail.com>
2024-01-18 10:35:11 +0100

    refactor: change release process
    #0

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,25 +3,50 @@ const fs = require('fs')
 module.exports = function (grunt) {
 	require('load-grunt-tasks')(grunt)
 	grunt.initConfig({
+		originalBranch: '',
+		version: '',
 		exec: {
 			lint: { cmd: 'npx eslint src' },
 			test: { cmd: 'npx jest --config jest-unit-config.json' },
 			tsc: { cmd: 'npx tsc' },
-			release: { cmd: './release.sh' },
-			to_develop: { cmd: './to_develop.sh' },
-			doc: { cmd: 'npx typedoc ' }
+			doc: { cmd: 'npx typedoc --plugin typedoc-plugin-markdown --out doc/source src/lib/index.ts' },
+			getOriginalBranch: {
+				cmd: 'git branch | sed -n -e \'s/^\\* \\(.*\\)/\\1/p\'',
+				callback: function (error, stdout, stderr) {
+					if (error) {
+						grunt.log.error(stderr)
+					} else {
+						grunt.config.set('originalBranch', stdout.trim())
+					}
+				}
+			},
+			standardVersion: {
+				cmd: 'standard-version'
+			},
+			gitFlowRelease: {
+				cmd: `git add . && git commit -m "chore(release): <%= version %>" && git push && git push --tags
+				  &&  git checkout -b release/<%= version %> && git push --set-upstream origin release/<%= version %>
+				  &&  git checkout main && git merge release/<%= version %> -m "chore(release): release <%= version %>" && git push
+				  &&  git checkout <%= originalBranch %> && git merge release/<%= version %> -m "chore(release): release <%= version %>" && git push
+				  &&  git branch -D release/<%= version %>
+				`
+			}
 		},
 		clean: {
-			dist: ['dist'],
-			build: ['build']
+			build: ['build'],
+			dist: ['dist']
 		},
 		copy: {
 			lib: { expand: true, cwd: 'build/lib', src: '**', dest: 'dist/' },
-			bin: { expand: true, cwd: 'src/lib/bin', src: '**', dest: 'dist/bin/' },
 			readme: { expand: true, src: './README.md', dest: 'dist/' },
+			changeLog: { expand: true, src: './CHANGELOG.md', dest: 'dist/' },
 			license: { expand: true, src: './LICENSE', dest: 'dist/' },
 			jest: { expand: true, src: './jest-unit-config.json', dest: 'dist/' }
 		}
+	})
+	grunt.registerTask('get-version', 'get version from package.json', function () {
+		const version = grunt.file.readJSON('./package.json').version
+		grunt.config.set('version', version)
 	})
 	grunt.registerTask('create-package', 'create package.json for dist', function () {
 		const data = require('./package.json')
@@ -31,13 +56,24 @@ module.exports = function (grunt) {
 			test: data.scripts.test
 		}
 		data.main = 'index.js'
-		data.bin = { 'config-extends': 'bin/config-extends' }
 		data.types = 'index.d.ts'
 		fs.writeFileSync('dist/package.json', JSON.stringify(data, null, 2), 'utf8')
 	})
-	grunt.registerTask('dist', ['clean:dist', 'clean:build', 'exec:tsc', 'exec:lint', 'exec:test', 'copy:lib', 'copy:bin', 'copy:jest', 'copy:readme', 'copy:license', 'create-package'])
-	grunt.registerTask('to_develop', ['dist', 'exec:to_develop'])
-	grunt.registerTask('release', ['dist', 'exec:release'])
+	grunt.registerTask('build-wiki', 'build wiki', function () {
+		const task = require('./build/dev/task/buildWiki')
+		task.apply(this.async())
+	})
+	grunt.registerTask('exec-release', ['exec:standardVersion', 'copy:changeLog', 'create-package', 'get-version', 'exec:gitFlowRelease'])
+	grunt.registerTask('run-release-if-applicable', 'run release if applicable', function () {
+		const originalBranch = grunt.config.get('originalBranch')
+		if (originalBranch === 'develop' || originalBranch.startsWith('hotfix')) {
+			grunt.task.run('exec-release')
+		} else {
+			grunt.log.writeln('Current branch ' + originalBranch + ', cannot release from branch different from develop or hotfix.')
+		}
+	})
+	grunt.registerTask('dist', ['clean:dist', 'clean:build', 'exec:tsc', 'exec:lint', 'exec:test', 'copy:lib', 'copy:jest', 'copy:readme', 'copy:license', 'create-package'])
 	grunt.registerTask('doc', ['exec:doc'])
+	grunt.registerTask('release', ['dist', 'doc', 'exec:getOriginalBranch', 'run-release-if-applicable'])
 	grunt.registerTask('default', [])
 }
